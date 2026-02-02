53a38ad4498f9b84e9f355b88acd3fcb57ceb878
SaumyaKumarDas <dassaumya13@gmail.com>
2026-02-02 18:17:34 +0545

    Splash Sign Up Sign In are Completed.

---
+++ lib/Widgets/uihelper.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class UiHelper {
+  static CustomTextButton({
+    required String text,
+    required VoidCallback callback,
+  }) {
+    return TextButton(
+      onPressed: () {
+        callback();
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 12, color: Color(0XFF3797EF)),
+      ),
+    );
+  }
+
+  static CustomTextField({
+    required TextEditingController controller,
+    required String text,
+    required bool tohide,
+    required TextInputType textinputtype,
+  }) {
+    return Container(
+      height: 50,
+      width: 343,
+      decoration: BoxDecoration(
+        color: Color(0XFF121212),
+        border: Border.all(color: Colors.white24),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: TextField(
+          controller: controller,
+          obscureText: tohide,
+          keyboardType: textinputtype,
+          decoration: InputDecoration(
+            hintText: text,
+            hintStyle: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+              color: Color(0XFFFFFFFF),
+            ),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  static CustomImage({required String imgurl}) {
+    return Image.asset("assets/images/$imgurl");
+  }
+  static CustomButton({required VoidCallback callback, required String buttonname}){
+    return SizedBox(
+      height: 45,
+      width : 343,
+      child: ElevatedButton(onPressed: (){
+        callback();
+      }, style: ElevatedButton.styleFrom(backgroundColor: Color(0XFF3797EF), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),child: Center(child: Text(buttonname, style: TextStyle(fontSize: 14, color: Colors.white),))),
+    );
+  }
+}
